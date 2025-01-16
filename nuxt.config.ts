f5f1c8c2d690a90b12cce0da0b59dea3b38a17a4
Name <email@email.ru>
2025-01-16 22:29:52 +0300

    Добавлены новые настройки для nuxt.config файла

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: [
-    '~/assets/css/fonts.css',
-    '~/assets/scss/global.scss',
-  ]
-})
 
+  app: {
+    // baseURL: process.env.NUXT_PUBLIC_API_BASE,
+  },
+
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL,
+    },
+  },
+
+  css: [
+    "~/assets/css/fonts.css",
+    "~/assets/scss/global.scss",
+    "~/assets/scss/global/variables.scss",
+  ],
+
+  modules: ["@nuxtjs/i18n", "@nuxtjs/google-fonts", "@pinia/nuxt"],
+
+  googleFonts: {
+    preconnect: true,
+    prefetch: true,
+    preload: true,
+    families: {
+      "Open+Sans": true,
+    },
+  },
+
+  i18n: {
+    baseUrl: "http://localhost:4000/api/v1/",
+    vueI18n: "./configs/i18n.config.ts",
+    locales: [
+      {
+        code: "ru",
+        name: "Русский",
+      },
+      {
+        code: "en",
+        name: "English",
+      },
+    ],
+  },
+});
