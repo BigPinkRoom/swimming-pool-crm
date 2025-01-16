7da5a4764da04f0425284e400a40a98ca8aad5d1
Name <email@email.ru>
2025-01-16 22:41:45 +0300

    Модернизирует страницу SignUp, добавляя ей новый функционал

--- pages/SignUp.vue
+++ pages/SignUp.vue
@@ -1,5 +1,36 @@
 <script setup>
+const { $services } = useNuxtApp();
+
+const branches = await $services.branches.get();
 </script>
 
 <template>
+  <div class="container">
+    <FormsSignUp
+      :branch-options-list="branchesSelectList"
+      @formSubmit="formSubmitHandler"
+    />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: calc(100vh - 4rem);
+
+  background-color: #ccc;
+}
+
+.title {
+  color: #eee;
+  font-size: 3.2rem;
+  font-family: Arial, sans-serif;
+
+  text-transform: uppercase;
+  text-decoration: none;
+}
+</style>
