9c64f6a02db50b6544aa255007956bff23494be4
NoName <you@example.com>
2025-01-26 16:08:46 +0300

    Добавляет загрузку списка филиалов в app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup>
+import { useBranchesStore } from "@/stores/branchStore";
+
+const { $services } = useNuxtApp();
+const branchesStore = useBranchesStore();
+
+const branchesValue = await $services.branchesSelect.get();
+branchesStore.set(branchesValue);
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
