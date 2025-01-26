55a988ee27dda53259aa37ff4236c1524123b688
NoName <you@example.com>
2025-01-26 16:24:33 +0300

    Переписывает компонент  Select на nuxt3

--- components/ui/Selects/Select.vue
+++ components/ui/Selects/Select.vue
@@ -1,12 +1,43 @@
+<script setup>
+import { uuid } from "vue-uuid";
+
+const props = defineProps({
+  id: {
+    type: [String, Number],
+  },
+  name: {
+    type: String,
+  },
+  placeholder: {
+    type: String,
+    default: () => "",
+  },
+  optionsList: {
+    type: Array,
+  },
+});
+
+const uuidV4 = uuid.v4();
+
+const updateValue = function (value) {
+  console.log("select update value", value);
+};
+</script>
+
 <template>
   <div class="select">
-    <select :id="id" :name="name" class="select__field" @input="updateValue($event.target.value)">
+    <select
+      :id="id"
+      :name="name"
+      class="select__field"
+      @input="updateValue($event.target.value)"
+    >
       <option v-if="placeholder" value="null" disabled selected>
         {{ placeholder }}
       </option>
       <option
         v-for="option in optionsList"
-        :key="`${option.value}_${uuid}`"
+        :key="`${option.value}_${uuidV4}`"
         :disabled="option.disabled"
         :selected="option.selected"
         :value="option.value"
@@ -18,39 +49,6 @@
   </div>
 </template>
 
-<script>
-import { uuid } from 'vue-uuid';
-
-export default {
-  name: 'Select',
-  props: {
-    id: {
-      type: [String, Number],
-    },
-    name: {
-      type: String,
-    },
-    placeholder: {
-      type: String,
-      default: () => '',
-    },
-    optionsList: {
-      type: Array,
-    },
-  },
-  data() {
-    return {
-      uuid: '',
-    };
-  },
-  mounted() {
-    this.uuid = uuid.v4();
-  },
-  methods: {
-    updateValue(value) {},
-  },
-};
-</script>
 <style lang="scss" scoped>
 .select {
   &__field {
