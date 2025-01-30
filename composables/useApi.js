2d02dba647e3f6776a8a6fab84e8f1d98e1f491e
NoName <you@example.com>
2025-01-30 14:59:12 +0300

    Добавляет логику показа ошибок к useApi

--- composables/useApi.js
+++ composables/useApi.js
@@ -1,12 +1,23 @@
 export async function useApi(urlPath, method, params) {
+  const { $i18n } = useNuxtApp();
+  const t = $i18n.t;
+
   const config = useRuntimeConfig();
   const baseUrlValue = config.public.baseUrl;
+
+  const { $showMessage, $showError } = useNuxtApp();
 
   try {
     const { data, error } = await useFetch(`${baseUrlValue}${urlPath}`, {
       method,
       body: params,
     });
+
+    if (error.value) {
+      $showError(
+        t(`forms.signup.validationErrors.${error.value?.data.error.message}`)
+      );
+    }
 
     return data?.value;
   } catch (error) {
