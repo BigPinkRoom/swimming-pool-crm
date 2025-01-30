4dad74b007c010e47e9978655be57af121ed9538
NoName <you@example.com>
2025-01-30 15:01:38 +0300

    Добавляет infoBar в лэйаут default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,43 @@
-<script setup></script>
+<script setup>
+import { useMessageStore } from "@/stores/messageStore";
+
+const messageStore = useMessageStore();
+</script>
 
 <template>
-  <HeaderNavbar />
-  <NuxtPage />
+  <div class="layout">
+    <div class="layout__infobar">
+      <InfoBar
+        v-for="message in messageStore.messages"
+        :id="message.id"
+        :key="message.id"
+        :type="message.type"
+      >
+        <template #message>
+          {{ message.message }}
+        </template>
+      </InfoBar>
+    </div>
+    <HeaderNavbar />
+    <NuxtPage />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.layout {
+  height: 100vh;
+
+  &__infobar {
+    position: fixed;
+    top: 5.1rem;
+
+    width: 100%;
+  }
+
+  &__navbar {
+  }
+
+  &__main {
+  }
+}
+</style>
