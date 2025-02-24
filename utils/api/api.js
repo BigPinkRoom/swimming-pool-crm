c1c3c9de7f2837a76b649df4e0331829369151aa
NoName <you@example.com>
2025-02-24 23:17:46 +0300

    Перемещает файлы сервисов и апи в корень

--- utils/api/api.js
+++
@@ -1,9 +0,0 @@
-import Branches from "./modules/branches";
-import User from "./modules/user.js";
-
-export default class Api {
-  constructor(app) {
-    this.branches = new Branches(app);
-    this.user = new User(app);
-  }
-}
