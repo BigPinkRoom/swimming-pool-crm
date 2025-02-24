4aae2c7cad250601bd3d91c76f6b17705f326a19
NoName <you@example.com>
2025-02-24 23:58:58 +0300

    Добавляет страницу входа

--- pages/SignIn.vue
+++ pages/SignIn.vue
@@ -1,5 +1,54 @@
 <script setup>
+import { useBranchesStore } from "@/stores/branchStore";
+
+const { $services } = useNuxtApp();
+const branchesSelectValues = useBranchesStore().branches;
+
+const formSubmitHandler = async (eventForm) => {
+  // const user = eventForm.target.elements;
+
+  try {
+    const result = await $services.user.signIn(eventForm);
+
+    return result;
+  } catch (err) {
+    throw err; // Пробрасываем ошибку для error
+  }
+};
+
+const refreshUser = async () => {
+  const data = await $services.user.getCurrent();
+};
 </script>
 
 <template>
+  <div class="container">
+    <FormsSignIn
+      :branch-options-list="branchesSelectValues"
+      @formSubmit="formSubmitHandler"
+    />
+    <button @click="refreshUser">get user</button>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: calc(100vh - 4rem);
+
+  background-color: #ccc;
+}
+
+.title {
+  color: #eee;
+  font-size: 3.2rem;
+  font-family: Arial, sans-serif;
+
+  text-transform: uppercase;
+  text-decoration: none;
+}
+</style>
