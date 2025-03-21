7e2ba23752baddbef1488354b7794d94392a5183
NoName <newitmessages@gmail.com>
2025-03-21 23:55:50 +0300

    Добавляет сущности и сервисы(новые функции) для user

--- entities/userEntity.js
+++ entities/userEntity.js
@@ -1,15 +1,135 @@
+import { userSignInValidationSchema } from "@/schemas/zod/userSchemas";
+
 export default class UserEntity {
-  constructor(context) {
+  constructor({ context }) {
     this.context = context;
+
     const { $i18n } = useNuxtApp();
     this.t = $i18n.t;
   }
 
-  createUserSignInModel(form) {
-    const formData = new FormData(form.target);
+  /**
+   * Форматирует ошибки валидации Zod в объект с ключами полей и сообщениями об ошибках.
+   * @param {Object} error - Ошибки валидации Zod.
+   * @returns {Object} Объект с ошибками в формате { fieldName: errorMessage }.
+   */
+  _formatValidationErrors(error) {
+    if (error.constructor.name === "_ZodError") {
+      const errors = error.errors.reduce((acc, curr) => {
+        const field = curr.path[0];
+        acc[field] = curr.message;
+        return acc;
+      }, {});
 
-    const email = formData.get("email");
-    console.log("email", email);
-    return formData;
+      return errors;
+    } else {
+      return error;
+    }
+  }
+
+  /**
+   * Валидирует поля из FormData с использованием указанной схемы Zod.
+   * @param {FormData} formData - Объект FormData, содержащий данные для валидации.
+   * @param {ZodSchema} schema - Схема валидации Zod.
+   * @returns {Object} Объект с валидированными данными.
+   */
+  _validateFields(formData, schema) {
+    try {
+      const unvalidatedFields = {};
+
+      for (const [key, value] of formData.entries()) {
+        unvalidatedFields[key] = value;
+      }
+
+      const validatedFields = schema.parse(unvalidatedFields);
+
+      return validatedFields;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * Устанавливает валидированные данные в текущий объект FormData.
+   * @param {FormData} formData - Исходный объект FormData.
+   * @param {Object} validatedFormData - Объект с валидированными данными.
+   * @returns {FormData} Новый объект FormData с валидированными данными.
+   */
+  _setValidatedFields(formData, validatedFormData) {
+    const newFormData = formData;
+    for (const [key, value] of Object.entries(validatedFormData)) {
+      newFormData.set(key, value);
+    }
+
+    return newFormData;
+  }
+  /**
+   * Создает модель данных для входа пользователя, валидируя и форматируя FormData.
+   * @param {FormData} formDataRaw - Исходный объект FormData с данными пользователя.
+   * @returns {FormData} Новый объект FormData с валидированными данными.
+   * @throws {Object} Объект с ошибками валидации в формате { fieldName: errorMessage }.
+   */
+  createUserSignInModel(formDataRaw) {
+    const formData = formDataRaw;
+    const schema = userSignInValidationSchema(this.t);
+
+    try {
+      const validatedFields = this._validateFields(formData, schema);
+
+      const validatedFormData = this._setValidatedFields(
+        formData,
+        validatedFields
+      );
+
+      return validatedFormData;
+    } catch (error) {
+      const errors = this._formatValidationErrors(error);
+
+      throw errors;
+    }
+  }
+
+  /**
+   * Создает модель ответа для входа пользователя на основе серверного ответа.
+   * @param {Object} response - Ответ сервера с данными пользователя.
+   * @returns {Object} Объект с данными пользователя в удобном формате.
+   */
+  createUserSignInResponseModel(response) {
+    return {
+      userId: response.user.user_id,
+      dataCreate: response.user.data_create,
+      email: response.user.email,
+      surname: response.user.surname,
+      name: response.user.name,
+      patronymic: response.user.patronymic,
+      userRole: response.user.user_role,
+      branch: response.user.branch,
+    };
+  }
+
+  /**
+   * Создает модель данных для регистрации пользователя, валидируя и форматируя FormData.
+   * @param {FormData} formDataRaw - Исходный объект FormData с данными пользователя для регистрации.
+   * @returns {FormData} Новый объект FormData с валидированными данными.
+   * @throws {Object} Объект с ошибками валидации в формате { fieldName: errorMessage }.
+   */
+  createUserSignUpModel(formDataRaw) {
+    const formData = formDataRaw;
+    const schema = userSignUpValidationSchema(this.t);
+
+    try {
+      const validatedFields = this._validateFields(formData, schema);
+
+      const validatedFormData = this._setValidatedFields(
+        formData,
+        validatedFields
+      );
+
+      return validatedFormData;
+    } catch (error) {
+      const errors = this._formatValidationErrors(error);
+
+      throw errors;
+    }
   }
 }
--- services/modules/user.js
+++ services/modules/user.js
@@ -5,10 +5,14 @@ import UserEntity from "@/entities/userEntity";
 export default class User {
   constructor(context) {
     this.context = context;
+
     const { $i18n } = useNuxtApp();
     this.t = $i18n.t;
+
     this.userStore = useUserStore();
     this.menusStore = useMenusStore();
+
+    this.userEntity = new UserEntity(this.context, new FormData());
   }
 
   async _setUser(user) {
@@ -19,8 +23,84 @@ export default class User {
     this.userStore.clear();
   }
 
+  _validateUserForm(eventSubmitForm) {
+    const currentFormData = new FormData(eventSubmitForm.target);
+
+    return this.userEntity.createUserSignInModel(currentFormData);
+  }
+
+  async _setMenu() {
+    try {
+      const menuValue = await this.context.$services.menus.getMainMenu();
+      this.menusStore.set(menuValue);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async _getSignInResponse(userForm) {
+    try {
+      return await this.context.$api.user.signIn(userForm);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrent(params) {
+    try {
+      const response = await this.context.$api.user.getCurrent();
+
+      return response;
+    } catch (error) {
+      if (error.value.statusCode === 401) {
+        return;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async _handleSuccessfulSignIn(response) {
+    const userSignInResponseModel =
+      this.userEntity.createUserSignInResponseModel(response);
+    this._setUser(userSignInResponseModel);
+
+    this._setMenu();
+
+    this.context.$showMessage(this.t("forms.signup"));
+
+    return userSignInResponseModel;
+  }
+
+  _handleSignInError(error) {
+    if (!error?.value?.data.error.message) {
+      throw error;
+    }
+
+    this.context.$showError(
+      this.t(
+        `forms.login.validationErrors.${error?.value?.data.error.message}`,
+        { userEmail: error.value?.data.error.userEmail }
+      )
+    );
+  }
+
+  async signIn(eventSubmitForm) {
+    try {
+      const userForm = this._validateUserForm(eventSubmitForm);
+
+      const response = await this._getSignInResponse(userForm);
+
+      return this._handleSuccessfulSignIn(response);
+    } catch (error) {
+      this._handleSignInError(error);
+    }
+  }
+
   async create(eventSubmitForm) {
-    const userForm = new UserEntity().createUserSignUpModel(eventSubmitForm);
+    const userForm = new UserEntity({
+      context: this.context,
+    }).createUserSignUpModel(eventSubmitForm);
 
     try {
       const response = await this.context.$api.user.signup(userForm);
@@ -44,42 +124,6 @@ export default class User {
       );
     }
   }
-  async signIn(eventSubmitForm) {
-    const userForm = new UserEntity().createUserSignInModel(eventSubmitForm);
-
-    try {
-      const response = await this.context.$api.user.signIn(userForm);
-
-      if (!response) return;
-
-      const userSignInResponseModel = {
-        userId: response.user.user_id,
-        dataCreate: response.user.data_create,
-        email: response.user.email,
-        surname: response.user.surname,
-        name: response.user.name,
-        patronymic: response.user.patronymic,
-        userRole: response.user.user_role,
-        branch: response.user.branch,
-      };
-
-      this._setUser(userSignInResponseModel);
-
-      this.context.$showMessage(this.t(`forms.signup`));
-
-      const menuValue = await this.context.$services.menus.getMainMenu();
-      this.menusStore.set(menuValue);
-
-      return userSignInResponseModel;
-    } catch (error) {
-      this.context.$showError(
-        this.t(
-          `forms.login.validationErrors.${error.value?.data.error.message}`,
-          { userEmail: error.value?.data.error.userEmail }
-        )
-      );
-    }
-  }
 
   async logout() {
     try {
@@ -91,17 +135,26 @@ export default class User {
     } catch (error) {}
   }
 
-  async getCurrent(params) {
-    try {
-      const response = await this.context.$api.user.getCurrent();
+  errorMessagesInputCheck(fieldName, formErrors, propsErrors) {
+    const checkFormsErrorsIsEmpty =
+      formErrors[fieldName] || propsErrors[fieldName] === "";
 
-      return response;
+    if (checkFormsErrorsIsEmpty) {
+      return formErrors[fieldName];
+    } else {
+      return propsErrors[fieldName];
+    }
+  }
+
+  validateField({ field, value, schemaRaw, formErrors, propsErrors }) {
+    const schema = schemaRaw(this.t);
+
+    try {
+      schema.pick({ [field]: true }).parse({ [field]: value });
+      formErrors[field] = ""; // Очищаем ошибку, если валидация прошла успешно
+      propsErrors[field] = "";
     } catch (error) {
-      if (error.value.statusCode === 401) {
-        return;
-      } else {
-        throw error;
-      }
+      formErrors[field] = error.errors[0].message; // Устанавливаем сообщение об ошибке
     }
   }
 }
