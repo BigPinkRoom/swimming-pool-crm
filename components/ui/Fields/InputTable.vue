c73b549896e4f699a35a5a2605cb48d95f7e5e6f
NoName <newitmessages@gmail.com>
2025-04-07 22:37:30 +0300

    Компонент обновлен под script setup

--- components/ui/Fields/InputTable.vue
+++ components/ui/Fields/InputTable.vue
@@ -1,43 +1,48 @@
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  id: {
+    type: [String, Number],
+  },
+  name: {
+    type: String,
+  },
+  placeholder: {
+    type: String,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: [String, Number],
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const onInput = (event) => {
+  emit("update:modelValue", event.target.value);
+};
+</script>
+
 <template>
   <div class="field">
     <input
       class="field__input"
       :id="id"
-      :value="value"
+      :value="modelValue"
       :type="type"
       :name="name"
       :placeholder="placeholder"
-      @input="updateValue($event.target.value)"
+      @input="onInput"
     />
   </div>
 </template>
 
-<script>
-export default {
-  name: 'FieldInput',
-  props: {
-    id: {
-      type: [String, Number],
-    },
-    name: {
-      type: String,
-    },
-    placeholder: {
-      type: String,
-    },
-    type: {
-      type: String,
-      required: true,
-    },
-    value: {
-      type: [String, Number],
-    },
-  },
-  methods: {
-    updateValue(value) {},
-  },
-};
-</script>
 <style lang="scss" scoped>
 .field {
   &__input {
