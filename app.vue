97e3afaf541e7ef2cd3d72a62c7843ef0ff97ba8
NoName <newitmessages@gmail.com>
2025-04-09 18:53:26 +0300

    Убирает логику родственников с app.vue

--- app.vue
+++ app.vue
@@ -2,10 +2,13 @@
 import { useBranchesStore } from "@/stores/branchStore";
 import { useMenusStore } from "@/stores/menusStore";
 import { useUserStore } from "@/stores/userStore";
+import { useRelativesStore } from "@/stores/relativeStore";
+import { onMounted } from "vue";
 
 const { $services } = useNuxtApp();
 const branchesStore = useBranchesStore();
 const menusStore = useMenusStore();
+const relativesStore = useRelativesStore();
 
 const branchesValue = await $services.branchesSelect.get();
 branchesStore.set(branchesValue);
