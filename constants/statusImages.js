eeb9dae49d0162d5aeb70095f6e903c62b409b7a
NoName <newitmessages@gmail.com>
2025-05-08 17:45:09 +0300

    Изменяет statusImages так, чтобы в названия были соответствующие цифры

--- constants/statusImages.js
+++ constants/statusImages.js
@@ -1,7 +1,7 @@
 export const statusImagesConstants = {
-  active: "/icons/statuses/active.svg",
-  freezed: "/icons/statuses/status_freezed.svg",
-  completed: "/icons/statuses/status_completed.svg",
-  cancelled: "/icons/statuses/status_cancelled.svg",
-  not_activated: "/icons/statuses/not_activated.svg",
+  1: "/icons/statuses/not_activated.svg",
+  2: "/icons/statuses/active.svg",
+  3: "/icons/statuses/status_completed.svg",
+  4: "/icons/statuses/status_freezed.svg",
+  5: "/icons/statuses/status_cancelled.svg",
 };
