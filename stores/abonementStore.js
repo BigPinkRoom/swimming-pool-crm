ec5fcfa8caec3e5846e2102148ca28d6bd498cb6
NoName <newitmessages@gmail.com>
2025-05-08 17:37:01 +0300

    Изменяет стор для абонементов

--- stores/abonementStore.js
+++ stores/abonementStore.js
@@ -2,16 +2,18 @@ import { defineStore } from "pinia";
 import { filterFilledObject } from "@/helpers/filterFilledObject.js";
 
 export const useAbonementsStore = defineStore("abonements", () => {
-  const abonements = ref({});
-
-  function set(payload) {
-    abonements.value = payload;
-  }
+  const abonements = ref([]);
 
   function setFilledObject(payload) {
-    const filteredObject = filterFilledObject(payload);
-    abonements.value = filteredObject;
+    if (Array.isArray(payload)) {
+      abonements.value = payload.map(filterFilledObject);
+    } else {
+      abonements.value = [];
+    }
   }
 
-  return { abonements, set, setFilledObject };
+  return {
+    abonements,
+    setFilledObject,
+  };
 });
