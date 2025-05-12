5fd26d0a053448238fdce67aa250066b1ab64772
NoName <newitmessages@gmail.com>
2025-05-12 22:27:09 +0300

    Добавляет логику relative by id

--- api/modules/relatives.js
+++ api/modules/relatives.js
@@ -12,4 +12,14 @@ export default class Clients {
       throw error;
     }
   }
+
+  async getRelativeById(id) {
+    try {
+      const response = await useApi("relatives/get-relative-by-id", "POST", id);
+
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
