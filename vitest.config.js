8594a75023d5605a5f42ffec0496d5f7ef841b39
NoName <newitmessages@gmail.com>
2025-05-19 23:46:31 +0300

    Изменяет конфигурацию (немного) vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -6,12 +6,20 @@ export default defineConfig({
   plugins: [vue()], // Добавляем плагин для обработки .vue файлов
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Если ваши файлы находятся в папке src
+      "@": path.resolve(__dirname, "."), // Указываем на корень проекта
     },
   },
   test: {
     globals: true,
     environment: "jsdom", // Эмуляция DOM для тестирования компонентов
     setupFiles: "./tests/setup.js", // Файл для глобальной настройки (опционально)
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+      "**/tests/end-to-end/**", // Исключаем директорию с e2e тестами
+    ],
   },
 });
